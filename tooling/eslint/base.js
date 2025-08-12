82d79806ac2f9d8b15664b9f3d839313cd7e9dc9
ccpu <m.doaie@hotmail.com>
2025-08-12 10:51:03 +0100

    fix: update dependencies in pnpm-workspace and eslint package
    - Bumped eslint to version 9.33.0
    - Updated prettier to version 3.6.2
    - Upgraded typescript to version 5.9.2
    - Updated @types/node to version 22.17.1
    - Upgraded rollup to version 4.46.2
    - Updated @pixpilot/eslint-config to version 2.3.0
    - Bumped eslint-plugin-turbo to version 2.5.5
    - Updated typescript-eslint to version 8.39.1

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -20,25 +20,6 @@ const baseConfig = makeConfig(
     plugins: {},
     rules: {},
   },
-  {
-    files: ['**/*.test.ts', '**/test/**/*.ts'],
-    rules: {
-      // Relax TypeScript rules
-      'ts/no-non-null-assertion': 'off',
-      'ts/no-unsafe-assignment': 'off',
-      'ts/no-unsafe-member-access': 'off',
-      'ts/no-unsafe-argument': 'off',
-      'ts/strict-boolean-expressions': 'off',
-      'ts/no-unsafe-return': 'off',
-      'no-console': 'off',
-      'prefer-const': 'off',
-      'ts/no-unsafe-call': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'ts/typedef': 'off',
-      'ts/no-explicit-any': 'off',
-      'ts/ban-ts-comment': 'off',
-    },
-  },
 );
 
 export default baseConfig;
